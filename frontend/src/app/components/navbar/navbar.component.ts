bcdbba4ce08894fe21f5b12d52b851e323ab2fda
RendonSofia1 <rendonsofi03@gmail.com>
2024-04-30 17:38:18 -0600

    navbar
    cambios para que el navbar cambie de color segun la pagina (avance)

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+  isLoginProfesorPage: boolean = false;
+  isHomePage: boolean = false;
+  isJuegoPage: boolean = false;
+  isProfesorPage: boolean = false;
+
+  //mostrar o no, botones
+  showBtnIngresar: boolean = true;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if(event instanceof NavigationEnd) {
+        //verifica que ruta esta activa
+        this.isHomePage = event.url === '/home';
+
+
+        //muestra botones
+        this.showBtnIngresar = !this.isJuegoPage;
+      }
+    })
+  }
 
 }
